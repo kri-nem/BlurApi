60cc2245a45901f0ec56a55fb7b0be904d3b6787
Kristóf Németh <nemethkristof1@gmail.com>
2024-04-14 18:47:42 +0200

    Make BlurImage endpoint async, accepting IFormFile & EncodingType as parameters, and returning Action result

--- BlurApiServer/BlurApiServer/Controllers/BlurApiController.cs
+++ BlurApiServer/BlurApiServer/Controllers/BlurApiController.cs
@@ -7,5 +7,11 @@ namespace BlurApiServer.Controllers
     [ApiController]
     public class BlurApiController : ControllerBase
     {
+        [HttpPost]
+        [Route("blur_image")]
+        public async Task<ActionResult> BlurImage(IFormFile uploadedFile, EncodingType encodingType)
+        {
+            return File(uploadedFile.OpenReadStream(), "image/jpeg");
+        }
     }
 }
