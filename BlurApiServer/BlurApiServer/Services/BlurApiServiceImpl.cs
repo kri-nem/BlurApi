12c6d42f8b59cb19f7b42e15b972c0a79c78ec40
Kristóf Németh <nemethkristof1@gmail.com>
2024-04-15 04:31:10 +0200

    Import shared object of libBlurApiService

--- BlurApiServer/BlurApiServer/Services/BlurApiServiceImpl.cs
+++ BlurApiServer/BlurApiServer/Services/BlurApiServiceImpl.cs
@@ -4,6 +4,10 @@ namespace BlurApiServer.Services;
 
 public class BlurApiServiceImpl : IBlurApiService
 {
+    [DllImport("/home/k/Development/BlurApi/BlurApiService/cmake-build-release/libBlurApiService.so",
+        EntryPoint = "process_image")]
+    private static extern int ProcessImageExternally(byte[] src, int srcSize, int encTy);
+
     public Stream ProcessImage(IFormFile uploadedFile, EncodingType encodingType)
     {
         uploadedFile.OpenReadStream();
