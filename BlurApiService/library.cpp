16f8afd4271c39d8850c9f03a0edd168251c36e6
Kristóf Németh <nemethkristof1@gmail.com>
2024-04-15 23:27:22 +0200

    Copy blurred and encoded image back to src

--- BlurApiService/library.cpp
+++ BlurApiService/library.cpp
@@ -4,7 +4,6 @@
 
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
 
 std::string getExtensionFor(EncodingType encodingType);
 
@@ -13,15 +12,13 @@ int process_image(unsigned char *src, int srcSize, EncodingType encodingType) {
     cv::Mat img = cv::imdecode({src, srcSize}, cv::IMREAD_COLOR);
     if (img.empty()) return 1;
     cv::GaussianBlur(img, img, {31, 31}, 0, 0);
-
-    // For testing purposes
-    /*
-    cv::imshow("Display window", img);
-    cv::waitKey(0);
-    */
-
     std::vector<unsigned char> buf;
     cv::imencode(getExtensionFor(encodingType), img, buf);
+
+    for(int i = 0; i < buf.size(); i++) {
+        src[i] = buf[i];
+    }
+
     return 0;
 
 }
