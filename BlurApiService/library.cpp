f16795e92ddde2404efe918bde67ce8186c4d9a5
Kristóf Németh <nemethkristof1@gmail.com>
2024-04-15 04:03:46 +0200

    Define process_image function with some testing code and helper function

--- BlurApiService/library.cpp
+++ BlurApiService/library.cpp
@@ -2,6 +2,35 @@
 
 #include <iostream>
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+std::string getExtensionFor(EncodingType encodingType);
+
+
+int process_image(unsigned char *src, int srcSize, EncodingType encodingType) {
+    cv::Mat img = cv::imdecode({src, srcSize}, cv::IMREAD_COLOR);
+    if (img.empty()) return 1;
+    cv::GaussianBlur(img, img, {31, 31}, 0, 0);
+
+    // For testing purposes
+    cv::imshow("Display window", img);
+    cv::waitKey(0);
+
+
+    std::vector<unsigned char> buf;
+    cv::imencode(getExtensionFor(encodingType), img, buf);
+    return 0;
+
+}
+
+std::string getExtensionFor(EncodingType encodingType) {
+    switch (encodingType) {
+        case JPEG:
+            return "jpeg";
+        case PNG:
+            return "png";
+        default: return "jpeg";
+    }
 }
